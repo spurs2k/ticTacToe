6b68fa24c6e558ccf8f165a7e26948d071404bfa
Dede Cantrell <spurs2k@gmail.com>
2023-04-28 22:27:34 -0500

    added logic for player moves

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 const App = {
-    // Selected HTML elements
+    // All of the Selected HTML elements
     $: {
         menu: document.querySelector('[data-id="menu"]'),
         menuItems: document.querySelector('[data-id="menu-items"]'),
@@ -12,10 +12,42 @@ const App = {
         moves: [],
     },
 
+    getGameStatus(moves) {
+
+        const p1Moves = moves.filter((move) => move.playerId === 1).map((move) => +move.squareId);
+        const p2Moves = moves.filter((move) => move.playerId === 2).map((move) => +move.squareId);
+
+        // Check for winner or tie game
+        const winningPatterns = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [1, 5, 9],
+            [3, 5, 7],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9],
+        ];
+
+        let winner = null;
+
+        winningPatterns.forEach(pattern => {
+            const p1Wins = pattern.every(v => p1Moves.includes(v));
+            const p2Wins = pattern.every(v => p2Moves.includes(v));
+
+            if (p1Wins) winner = 1;
+            if (p2Wins) winner = 2;
+        });
+
+        return {
+            status: moves.length === 9 || winner != null ? "complete" : "in-progress", // in-progress or complete?
+            winner, // 1 | 2 | null
+        };
+    },
+
     // ES6 syntax shorthand way to define a function property on App object. Placing the event listiner here allows me to control when I initialize the app
     init() {
         App.registerEventListeners();
-
     },
 
     registerEventListeners() {
@@ -34,8 +66,13 @@ const App = {
         App.$.squares.forEach((square) => {
             square.addEventListener("click", (event) => {
 
-                // Check if there is already a play, and if so, return early. square instead of event to avoid multiple plays per square
-                if (square.hasChildNodes()) {
+                // Check if there is already a play, and if so, return early.
+                const hasMove = (squareId) => {
+                    const existingMove = App.state.moves.find((move) => move.squareId === squareId);
+                    return existingMove !== undefined;
+                };
+                
+                if (hasMove(+square.id)) {
                     return;
                 };
 
@@ -54,29 +91,21 @@ const App = {
 
                 App.state.moves.push({
                     squareId: +square.id,
-                    playerId: currentPlayer
+                    playerId: currentPlayer,
                 });
-
-                App.state.currentPlayer = currentPlayer === 1 ? 2 : 1
-
-                console.log(App.state);
-
 
                 square.replaceChildren(icon);
 
-                // Check for winner or tie game
-                const winningPatterns = [
-                    [1, 2, 3],
-                    [4, 5, 6],
-                    [7, 8, 9],
-                    [1, 5, 9],
-                    [3, 5, 7],
-                    [1, 4, 7],
-                    [2, 5, 8],
-                    [3, 6, 9],
+                // Check if there is a winner or tie game
+                const game = App.getGameStatus(App.state.moves);
 
-                ];
-
+                if (game.status === "complete") {
+                    if (game.winner) {
+                        alert(`Player ${game.winner} wins!`);
+                    } else {
+                        alert("Tie");
+                    }
+                }
 
             });
         });
